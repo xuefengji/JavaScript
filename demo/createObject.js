242c6fe05d542eb088fa4883925c60b25b3e0eb9
xuefengji <jxfvip@sina.com>
2020-07-01 20:57:50 +0800

    docs: update

--- demo/createObject.js
+++ demo/createObject.js
@@ -1,5 +1,5 @@
 // 方式一：使用{}创建
-// o = {}
+// var o = {}
 // o.name = 'Tom';
 // o.age = '20';
 // console.log(o instanceof Object);
@@ -7,7 +7,7 @@
 // console.log(o.age);
 
 // 方式二：使用new Object()创建
-// o = new Object()
+// var o = new Object()
 // console.log(o instanceof Object)
 // o.name = 'Tom'
 // o.sysname = function(){
@@ -17,7 +17,30 @@
 
 
 //方式三：使用字面量方式创建
-o = {name:'Tom'}
+// var o = {name:'Tom'}
+// console.log(o instanceof Object)
+// o.age = 20
+// console.log(o.age)
+
+
+//使用工厂模式创建
+function createObject(){
+
+}
+
+
+//方式五：使用构造函数创建
+function Person(name){
+    this.name = name
+}
+o = new Person('Tom');
 console.log(o instanceof Object)
-o.age = 20
-console.log(o.age)
+o.sysName = function (){
+    console.log(this.name)
+}
+o.sysName()
+
+// 方式六：使用原型方式创建
+
+
+// 方式七：使用原型+构造函数方式创建
