97aa9eced613e543a202e716bff4e232ff91fb8c
xuefengji <jxfvip@sina.com>
2020-07-05 15:05:37 +0800

    docs: update

--- demo/promise.js
+++ demo/promise.js
@@ -1,3 +1,8 @@
-//回调函数
 
-
+let p = new Promise(function(resolve, reject){
+    //做一些异步操作
+    setTimeout(function(){
+        console.log('执行完成Promise');
+        resolve('要返回的数据可以任何数据例如接口返回数据');
+    }, 2000);
+});
