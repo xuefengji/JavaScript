13de56150bfe76b8000b847ac16055bfa7d5f620
@xuefeng.ji <jxfvip@sina.com>
2020-07-08 16:28:30 +0800

    docs: update

--- demo/promise.js
+++ demo/promise.js
@@ -6,3 +6,11 @@ let p = new Promise(function(resolve, reject){
         resolve('要返回的数据可以任何数据例如接口返回数据');
     }, 2000);
 });
+
+
+//使用一个函数包裹 Promise 并返回该 Promise 对象
+function automator(){
+    return new Promise(function(resolve, reject){
+        
+    })
+}
